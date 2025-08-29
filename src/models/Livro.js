0d8d7c4a6a2cadccd0b00b0fe08db915d1cb728c
JoannaFerreira <joannaferreira098@gmail.com>
2025-08-29 16:54:07 -0300

    refatoração

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,13 +1,18 @@
 import mongoose, {mongo} from "mongoose";
+import { autorSchema } from "./Autor.js"
 
 const livrosSchema =  new mongoose.Schema({
     id: {type: mongoose.Types.ObjectId},
     titulo: {type: String, required: true},
     editora: {type: String},
     preco:{type: Number},
-    paginas:{type: Number}
+    paginas:{type: Number},
+    autor: autorSchema
 }, {versionKey: false });
 
 const livro = mongoose.model("livros", livrosSchema);
+
+
+
 
 export default livro;
---
+++ src/routes/autoresRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import LivroController from "../controllers/livroController.js";
+import AutorController from "../controllers/autorController.js"
+
+const routes = express.Router();
+
+routes.get("/autor", AutorController.listarAutores);
+routes.get("/autor/:id", AutorController.listarAutorPorId);
+routes.post("/autor", AutorController.cadastrarAutor);
+routes.put("/autor/:id", AutorController.atualizarAutor);
+routes.delete("/autor/:id", AutorController.excluirAutor);
+
+
+export default routes;
+
